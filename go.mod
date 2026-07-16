1c5ae05c290094fbdb8077c912d94706e00ca2fc
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2026-07-16 22:27:41 +0300

    feat(auth): Added auth deps and validated auth config

--- go.mod
+++ go.mod
@@ -54,6 +54,7 @@ require (
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.6 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.10.0 // indirect
@@ -111,6 +112,7 @@ require (
 	golang.org/x/crypto v0.54.0 // indirect
 	golang.org/x/mod v0.37.0 // indirect
 	golang.org/x/net v0.56.0 // indirect
+	golang.org/x/oauth2 v0.36.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.40.0 // indirect
---
+++ internal/app/auth/config.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/Andriy-Sydorenko/repo-release-notifier/internal/shared/config"
+)
+
+const minJWTSecretLen = 32
+
+type OAuthProvider struct {
+	ClientID     string
+	ClientSecret string
+}
+
+func (p OAuthProvider) Enabled() bool { return p.ClientID != "" && p.ClientSecret != "" }
+
+type Config struct {
+	JWTSecret           string
+	SessionTTL          time.Duration
+	Secure              bool
+	Google              OAuthProvider
+	GitHub              OAuthProvider
+	RedirectBase        string
+	AccessRequestEmail  string
+	EnforceTokenVersion bool
+}
+
+func LoadConfig() Config {
+	return Config{
+		JWTSecret:           config.GetEnvOrDefault("JWT_SECRET", ""),
+		SessionTTL:          config.GetEnvDuration("SESSION_TTL", 24*time.Hour),
+		Secure:              config.GetEnvOrDefault("COOKIE_SECURE", "true") == "true",
+		Google:              OAuthProvider{config.GetEnvOrDefault("GOOGLE_CLIENT_ID", ""), config.GetEnvOrDefault("GOOGLE_CLIENT_SECRET", "")},
+		GitHub:              OAuthProvider{config.GetEnvOrDefault("GITHUB_OAUTH_CLIENT_ID", ""), config.GetEnvOrDefault("GITHUB_OAUTH_CLIENT_SECRET", "")},
+		RedirectBase:        config.GetEnvOrDefault("OAUTH_REDIRECT_BASE", config.GetEnvOrDefault("BASE_URL", "")),
+		AccessRequestEmail:  config.GetEnvOrDefault("ACCESS_REQUEST_EMAIL", ""),
+		EnforceTokenVersion: config.GetEnvOrDefault("ENFORCE_TOKEN_VERSION", "false") == "true",
+	}
+}
+
+func (c Config) Validate() error {
+	var errs []error
+	if len(c.JWTSecret) < minJWTSecretLen {
+		errs = append(errs, fmt.Errorf("JWT_SECRET must be at least %d chars", minJWTSecretLen))
+	}
+	if c.SessionTTL <= 0 {
+		errs = append(errs, errors.New("SESSION_TTL must be positive"))
+	}
+	return errors.Join(errs...)
+}
